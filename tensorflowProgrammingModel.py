9dc28bb120688162297755974ad260a356c225f7
5hseok <ohhs1010@gmail.com>
2023-05-30 21:53:18 +0900

    재업로드

--- tensorflowProgrammingModel.py
+++ tensorflowProgrammingModel.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 b=tf.Variable(tf.zeros((100,)))
 W=tf.Variable(tf.random_uniform((784,100),-1,1))
 x=tf.placeholder(tf.float32,(100,784))
-h=tf.nn.relu(tf.matmul(x,W)+b)
+h=tf.nn.relu(tf.matmupusl(x,W)+b)
